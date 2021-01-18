6913102b4062c4314fd37222f7da44812126783f
ismailaa123 <77631310+ismailaa123@users.noreply.github.com>
2021-01-18 16:03:09 +0000

    Update Travis-Security-system.py

--- Travis-Security-system.py
+++ Travis-Security-system.py
@@ -1,9 +1,17 @@
-known_users=["Dania","ismail","Mom","Dad"]
+#known users are the user which the system recognizes
 
+known_users=["Ana","Brian","Cletus","Debbie"]
+
+#This will print the legnth of the list
 print(len(known_users))
 
+#input method is used to let the user enter the information when the program is executed. 
+#*** Important thing here to take note is when you use input method always remember that the type is always in string even if you enter numberical values.
+#strip() eliminates any extra spaces and Capitalize() bring only the first alphabet in upper case mode
 
 name=input("what is your name?: ").strip().capitalize()
+
+#remove() is an inbuilt function in Python that removes a given object from the list.
 
 if name in known_users:
     print("Hello {}!".format(name))
@@ -19,3 +27,4 @@ else:
         if add_me=="y":
             known_users.append(name)
             
+#The append() method in python adds a single item to the existing list. It doesn't return a new list of items but will modify the original list by adding the item to the end of the list
